61f7345b9532da142c8c5b6c5a48d8d8f24090db
Josh <josh@joshthomas.dev>
2024-02-17 22:26:52 -0600

    remove type hints from test

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -8,12 +8,12 @@ import pytest
 from django_twc_toolbox.decorators import req
 
 
-def test_req(monkeypatch: pytest.MonkeyPatch) -> None:
+def test_req(monkeypatch):
     monkeypatch.setattr("django.conf.settings.DEBUG", False)
     url = "https://example.com/test-hc"
 
     @req(url)
-    def foo() -> None:
+    def foo():
         pass
 
     with mock.patch.object(httpx, "get") as mock_httpx_get:
@@ -23,11 +23,11 @@ def test_req(monkeypatch: pytest.MonkeyPatch) -> None:
 
 
 @pytest.mark.parametrize("url", ["", "invalid"])
-def test_req_invalid_url(url, caplog) -> None:
+def test_req_invalid_url(url, caplog):
     with caplog.at_level("ERROR"):
 
         @req(url)
-        def foo() -> None:
+        def foo():
             pass
 
     assert "Invalid URL" in caplog.text
