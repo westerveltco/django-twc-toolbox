aeccd3f448f618bdd5fdea7a19876e1e981e9e75
Josh <josh@joshthomas.dev>
2024-07-10 11:13:16 -0500

    move types to separate module

--- src/django_twc_toolbox/admin.py
+++ src/django_twc_toolbox/admin.py
@@ -2,70 +2,53 @@ from __future__ import annotations
 
 import sys
 from typing import Generic
-from typing import TypeVar
 
 from django.contrib import admin
 from django.contrib.admin.options import BaseModelAdmin
-from django.db.models.base import Model
 from django.http import HttpRequest
 
-if sys.version_info >= (3, 10):
-    from typing import TypeAlias
-else:
-    from typing_extensions import TypeAlias
+from django_twc_toolbox.types import ChildModelT
+from django_twc_toolbox.types import ListOrTuple
+from django_twc_toolbox.types import ParentModelT
 
 if sys.version_info >= (3, 12):
     from typing import override
 else:
     from typing_extensions import override
 
-_K = TypeVar("_K")
-
-if sys.version_info >= (3, 10):
-    _ListOrTuple: TypeAlias = list[_K] | tuple[_K, ...] | tuple[()]
-else:
-    from typing import List
-    from typing import Tuple
-    from typing import Union
-
-    _ListOrTuple: TypeAlias = Union[List[_K], Tuple[_K, ...], Tuple[()]]
-
-_ChildModelT = TypeVar("_ChildModelT", bound=Model)
-_ParentModelT = TypeVar("_ParentModelT", bound=Model)
-
 
 class ReadOnlyModelAdmin(
-    Generic[_ChildModelT, _ParentModelT], BaseModelAdmin[_ChildModelT]
+    Generic[ChildModelT, ParentModelT], BaseModelAdmin[ChildModelT]
 ):
     can_delete = False
     extra = 0
 
     @override
     def get_readonly_fields(
-        self, request: HttpRequest, obj: _ChildModelT | None = None
-    ) -> _ListOrTuple[str]:
+        self, request: HttpRequest, obj: ChildModelT | None = None
+    ) -> ListOrTuple[str]:
         return [f.name for f in self.model._meta.fields]
 
     @override
     def has_add_permission(  # type: ignore[override]
-        self, request: HttpRequest, obj: _ParentModelT | None
+        self, request: HttpRequest, obj: ParentModelT | None
     ) -> bool:
         return False
 
     @override
     def has_change_permission(  # type: ignore[override]
-        self, request: HttpRequest, obj: _ParentModelT | None = None
+        self, request: HttpRequest, obj: ParentModelT | None = None
     ) -> bool:
         return False
 
 
 class ReadOnlyStackedInline(
-    ReadOnlyModelAdmin[_ChildModelT, _ParentModelT],
-    admin.StackedInline[_ChildModelT, _ParentModelT],
+    ReadOnlyModelAdmin[ChildModelT, ParentModelT],
+    admin.StackedInline[ChildModelT, ParentModelT],
 ): ...
 
 
 class ReadOnlyTabularInline(
-    ReadOnlyModelAdmin[_ChildModelT, _ParentModelT],
-    admin.TabularInline[_ChildModelT, _ParentModelT],
+    ReadOnlyModelAdmin[ChildModelT, ParentModelT],
+    admin.TabularInline[ChildModelT, ParentModelT],
 ): ...
