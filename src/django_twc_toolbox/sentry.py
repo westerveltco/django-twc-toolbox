48f0f5c167d98c8f69e52f2b5a00ff289dae1f00
Josh <josh@joshthomas.dev>
2024-07-23 16:39:31 -0500

    add type hint for `sampling_context` in sentry functions

--- src/django_twc_toolbox/sentry.py
+++ src/django_twc_toolbox/sentry.py
@@ -1,23 +1,25 @@
 from __future__ import annotations
 
+from sentry_sdk._types import SamplingContext
+
 from django_twc_toolbox.conf import app_settings
 
 
-def sentry_traces_sampler(sampling_context):
+def sentry_traces_sampler(sampling_context: SamplingContext):
     if _should_disgard(sampling_context):
         return 0
 
     return app_settings.SENTRY_TRACES_RATE
 
 
-def sentry_profiles_sampler(sampling_context):
+def sentry_profiles_sampler(sampling_context: SamplingContext):
     if _should_disgard(sampling_context):
         return 0
 
     return app_settings.SENTRY_PROFILE_RATE
 
 
-def _should_disgard(sampling_context) -> bool:
+def _should_disgard(sampling_context: SamplingContext) -> bool:
     DISGARDED_METHODS = app_settings.SENTRY_DISGARDED_METHODS
     DISGARDED_PATHS = app_settings.SENTRY_DISGARDED_PATHS
 
