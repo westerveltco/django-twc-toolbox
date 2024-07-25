273fe2e19e6964636423c8d2a59abef0c17e08dd
Josh <josh@joshthomas.dev>
2024-07-25 15:31:17 -0500

    flesh out tests with args

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -29,16 +29,25 @@ def urlconf():
 
 
 @pytest.mark.parametrize(
-    "viewname,kwargs,query,fragment,expected",
+    "viewname,args,kwargs,query,fragment,expected",
     [
-        ("view", None, None, None, "/view/"),
-        (view, None, None, None, "/view/"),
-        ("view_with_arg", {"arg": 123}, None, None, "/view_with_arg/123/"),
-        ("view", None, {"param": "hello"}, None, "/view/?param=hello"),
-        ("view", None, None, "fragment", "/view/#fragment"),
-        ("view", None, {"param": "hello"}, "fragment", "/view/?param=hello#fragment"),
+        ("view", None, None, None, None, "/view/"),
+        (view, None, None, None, None, "/view/"),
+        ("view_with_arg", (123,), None, None, None, "/view_with_arg/123/"),
+        ("view_with_arg", None, {"arg": 123}, None, None, "/view_with_arg/123/"),
+        ("view", None, None, {"param": "hello"}, None, "/view/?param=hello"),
+        ("view", None, None, None, "fragment", "/view/#fragment"),
         (
             "view",
+            None,
+            None,
+            {"param": "hello"},
+            "fragment",
+            "/view/?param=hello#fragment",
+        ),
+        (
+            "view",
+            None,
             None,
             {"param": "hello++"},
             "fragment&more",
@@ -47,27 +56,32 @@ def urlconf():
         (
             "view",
             None,
+            None,
             {"params": ["hello", "world"]},
             None,
             "/view/?params=%5B%27hello%27%2C+%27world%27%5D",
         ),
-        ("view", None, {"param": ""}, None, "/view/?param="),
+        ("view", None, None, {"param": ""}, None, "/view/?param="),
     ],
 )
-def test_reverse(viewname, kwargs, query, fragment, expected):
-    assert reverse(viewname, kwargs=kwargs, query=query, fragment=fragment) == expected
+def test_reverse(viewname, args, kwargs, query, fragment, expected):
+    assert (
+        reverse(viewname, args=args, kwargs=kwargs, query=query, fragment=fragment)
+        == expected
+    )
 
 
 @pytest.mark.parametrize(
-    "viewname,kwargs,query,fragment,expected",
+    "viewname,args,kwargs,query,fragment,expected",
     [
-        ("view", {"unexpected": "param"}, None, None, NoReverseMatch),
-        ("view_with_arg", None, None, None, NoReverseMatch),
-        ("view_with_arg", {"arg": "incorrect_type"}, None, None, NoReverseMatch),
-        ("view", None, {"param": None}, None, TypeError),
-        ("nonexistent_view", None, None, None, NoReverseMatch),
+        ("view", None, {"unexpected": "param"}, None, None, NoReverseMatch),
+        ("view_with_arg", None, None, None, None, NoReverseMatch),
+        ("view_with_arg", "incorrect_type", None, None, None, NoReverseMatch),
+        ("view_with_arg", None, {"arg": "incorrect_type"}, None, None, NoReverseMatch),
+        ("view", None, None, {"param": None}, None, TypeError),
+        ("nonexistent_view", None, None, None, None, NoReverseMatch),
     ],
 )
-def test_reverse_invalid(viewname, kwargs, query, fragment, expected):
+def test_reverse_invalid(viewname, args, kwargs, query, fragment, expected):
     with pytest.raises(expected):
-        reverse(viewname, kwargs=kwargs, query=query, fragment=fragment)
+        reverse(viewname, args=args, kwargs=kwargs, query=query, fragment=fragment)
