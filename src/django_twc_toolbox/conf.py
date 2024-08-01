168ec4a0da207c372bb27662f5fbdc5932a8e6e7
Josh <josh@joshthomas.dev>
2024-08-01 14:54:13 -0500

    bump template to 2024.24

--- src/django_twc_toolbox/conf.py
+++ src/django_twc_toolbox/conf.py
@@ -11,17 +11,6 @@ DJANGO_TWC_TOOLBOX_SETTINGS_NAME = "DJANGO_TWC_TOOLBOX"
 
 @dataclass(frozen=True)
 class AppSettings:
-    CACHE_TIME_ROBOTS_TXT = 60 * 60 * 24  # one day
-    CACHE_TIME_SECURITY_TXT = 60 * 60 * 24  # one day
-    TEMPLATE_404 = "404.html"
-    TEMPLATE_500 = "500.html"
-    TEMPLATE_ROBOTS_TXT = "robots.txt"
-    TEMPLATE_SECURITY_TXT = ".well-known/security.txt"
-    SENTRY_DISGARDED_METHODS = ["GET", "HEAD"]
-    SENTRY_DISGARDED_PATHS = ["/health/"]
-    SENTRY_PROFILE_RATE = 0.5
-    SENTRY_TRACES_RATE = 0.5
-
     @override
     def __getattribute__(self, __name: str) -> object:
         user_settings = getattr(settings, DJANGO_TWC_TOOLBOX_SETTINGS_NAME, {})
