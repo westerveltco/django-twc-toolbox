44f1a19195d716dc84dfcccce73a621495c245be
Josh <josh@joshthomas.dev>
2024-08-30 10:50:58 -0500

    add type annotations

--- src/django_twc_toolbox/middleware.py
+++ src/django_twc_toolbox/middleware.py
@@ -1,19 +1,22 @@
 from __future__ import annotations
 
+from typing import Callable
 from urllib.parse import urlparse
 from urllib.parse import urlunparse
 
+from django.http import HttpRequest
 from django.http import HttpResponsePermanentRedirect
+from django.http.response import HttpResponseBase
 
 
 class WwwRedirectMiddleware:
     # all credit to Adam Johnson for the inspiration
     # https://adamj.eu/tech/2020/03/02/how-to-make-django-redirect-www-to-your-bare-domain/
 
-    def __init__(self, get_response):
+    def __init__(self, get_response: Callable[[HttpRequest], HttpResponseBase]) -> None:
         self.get_response = get_response
 
-    def __call__(self, request):
+    def __call__(self, request: HttpRequest) -> HttpResponseBase:
         url = urlparse(request.build_absolute_uri())
 
         if url.netloc.startswith("www."):
