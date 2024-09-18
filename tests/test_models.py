047b372437a77c581f83938a0f53dbbe828ca9ea
Josh Thomas <jthomas@westervelt.com>
2024-09-18 10:17:40 -0500

    remove `changed_by` field from `WithHistory` abstract model (#119)

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,7 +5,6 @@ from unittest.mock import patch
 
 import pytest
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.test import override_settings
 from model_bakery import baker
 
@@ -43,75 +42,6 @@ def test_save_without_history_method():
     dummy.save_without_history()
 
     assert dummy.history.count() == 1
-
-
-def test_changed_by_user():
-    user = baker.make(get_user_model())
-
-    dummy = baker.make("dummy.ModelWithHistory", changed_by=user)
-
-    assert dummy.changed_by == user
-    assert dummy.history.count() == 1
-    assert dummy.history.first().history_user == user
-
-
-def test_change_user():
-    user = baker.make(get_user_model())
-    another_user = baker.make(get_user_model())
-
-    dummy = baker.make("dummy.ModelWithHistory", changed_by=user)
-
-    assert dummy.changed_by == user
-    assert dummy.history.count() == 1
-
-    dummy.changed_by = another_user
-    dummy.save()
-
-    assert dummy.changed_by == another_user
-    assert dummy.history.count() == 2
-    assert dummy.history.first().history_user == another_user
-
-
-def test_history_user_property():
-    user = baker.make(get_user_model())
-
-    dummy = baker.make("dummy.ModelWithHistory", changed_by=user)
-
-    assert dummy._history_user == user
-
-
-def test_history_user_setter():
-    user = baker.make(get_user_model())
-    another_user = baker.make(get_user_model())
-
-    dummy = baker.make("dummy.ModelWithHistory", changed_by=user)
-
-    dummy._history_user = another_user
-    dummy.save()
-
-    assert dummy.changed_by == another_user
-    assert dummy.history.count() == 2
-    assert dummy.history.first().history_user == another_user
-
-
-def test_multiple_changes_with_different_users():
-    user = baker.make(get_user_model())
-    another_user = baker.make(get_user_model())
-
-    dummy = baker.make("dummy.ModelWithHistory", changed_by=user)
-
-    dummy.name = "Updated by first user"
-    dummy.save()
-
-    dummy._history_user = another_user
-    dummy.name = "Updated by second user"
-    dummy.save()
-
-    assert dummy.history.count() == 3
-    historical_records = list(dummy.history.all())
-    assert historical_records[0].history_user == another_user
-    assert historical_records[1].history_user == user
-    assert historical_records[2].history_user == user
 
 
 @override_settings(
